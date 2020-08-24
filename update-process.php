3307e8a6931d0d029796b4155ba01632971f5dc4
milivojevic-dev <60219434+milivojevic-dev@users.noreply.github.com>
2020-08-24 13:09:21 +0200

    Update update-process.php

--- update-process.php
+++ update-process.php
@@ -40,7 +40,7 @@ $result = mysqli_query($con, $sql);
                     <a class="nav-link" href="about-us.php">About Us</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="#">Real Estate</a>
+                    <a class="nav-link" href="real-estate.php">Real Estate</a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" href="contact.php">Contact</a>
